f51efeaad3043e7f6c05e2ac23f97153659a6be2
Jordi Gallardo Sánchez <jordigallardo0621@gmail.com>
2025-07-02 19:35:46 +0200

    Añadido Logo, hecho responsive y añadido Terminos Condiciones,...

--- js/script.js
+++ js/script.js
@@ -13,3 +13,38 @@ document.querySelectorAll('.nav-links a').forEach(anchor => {
     }
   });
 });
+
+
+// Seleccionamos todos los links con data-modal
+document.querySelectorAll('a[data-modal]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const modalId = link.getAttribute('data-modal');
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.classList.add('active'); // mostramos el modal
+      document.body.style.overflow = 'hidden'; // para evitar scroll del fondo
+    }
+  });
+});
+
+// Seleccionamos todos los botones cerrar modales
+document.querySelectorAll('.custom-modal-close').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modal = btn.closest('.custom-modal');
+    modal.classList.remove('active');
+    document.body.style.overflow = ''; // restauramos scroll fondo
+  });
+});
+
+// También cerrar modal al hacer click fuera del contenido
+document.querySelectorAll('.custom-modal').forEach(modal => {
+  modal.addEventListener('click', e => {
+    if (e.target === modal) { // solo si clicas en el fondo oscuro
+      modal.classList.remove('active');
+      document.body.style.overflow = '';
+    }
+  });
+});
+
+
